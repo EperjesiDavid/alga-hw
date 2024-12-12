1f6bc4256c82561949bf87e1b9ee78f5668de31d
davideperjesi <48140421+EperjesiDavid@users.noreply.github.com>
2024-12-13 00:28:25 +0100

    Update megoldas.py

--- keresés/megoldas.py
+++ keresés/megoldas.py
@@ -1,43 +1,18 @@
-#!/bin/python3
+def count_rounds(n, arr):
+    # Tároljuk az egyes számok pozícióit
+    positions = [0] * n
+    for i, num in enumerate(arr):
+        positions[num - 1] = i
 
-import math
-import os
-import random
-import re
-import sys
+    # Számláljuk a köröket
+    rounds = 1
+    for i in range(1, n):
+        if positions[i] < positions[i - 1]:
+            rounds += 1
+    print(positions)
+    return rounds
 
-#
-# Complete the 'pairs' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. INTEGER k
-#  2. INTEGER_ARRAY arr
-#
-
-def pairs(k, arr):
-
-    num_set = set(arr)
-    count = 0
-    for x in arr:
-        if x + k in num_set:
-            count += 1
-    return count
-
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    first_multiple_input = input().rstrip().split()
-
-    n = int(first_multiple_input[0])
-
-    k = int(first_multiple_input[1])
-
-    arr = list(map(int, input().rstrip().split()))
-
-    result = pairs(k, arr)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+# Példa input
+n = int(input())
+arr = list(map(int, input().split()))
+print(count_rounds(n, arr))
